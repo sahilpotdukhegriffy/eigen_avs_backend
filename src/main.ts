afe24e46c1b8f7d8a5164157f2c12012ccdea543
sahilpotdukhegriffy <sahilpotdukhe@griffy.app>
2024-12-08 09:08:36 +0530

    CORS enabled

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,26 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,HEAD,PUT,PATCH,POST,DELETE',
+      );
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      return res.sendStatus(204); // Preflight response with no content
+    }
+    next();
+  });
 
   // Enable CORS
   app.enableCors({
     origin: ['http://localhost:5173'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
+    allowedHeaders: 'Content-Type, Authorization', // Allow specific headers
   });
 
   await app.listen(process.env.PORT || 3000);
